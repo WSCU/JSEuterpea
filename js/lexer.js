a4c4630c9a0b48fb3a10e87ba400fed8710a1941
Anthoney B. Arkadie <milordog1@hotmail.com>
2016-01-30 17:10:49 -0700

    Put in JS syntax
    took off typing and messed around with stuff until I stopped getting red
    bars.  No idea if it will work or not

--- js/lexer.js
+++ js/lexer.js
@@ -1,4 +1,4 @@
-package music;
+//package music;
 
 import static music.TokenType.*;
 
@@ -6,13 +6,13 @@ import static music.TokenType.*;
  * The Functional Music project
  * @author Western State College, CIS412 class
  */
-public class Lexer {
+var Lexer = {
 
-    
     //public ArrayList<LexerError> error = new ArrayList<LexerError>();
-    public static final String opChars = "!&|=><+-.*^%/";
 
-    /**
+    OP_CHARS : "!&|=><+-.*^%/" }
+
+    /*
      * @param program
      * @param fname
      * @param string The string that is lexed
@@ -27,29 +27,31 @@ public class Lexer {
      * letters being saved in a token stream called vartoken
      * digits being saved in a token stream called numbertoken
      * punctuation such as [](),;{}_ being saved in a token stream called puncToken
-     * oporator such as !&|=><+- saved in the variable opChars being saved in a token
+     * oporator such as !&|=><+- saved in the variable OP_CHARS being saved in a token
      * @return
      * stream called opToken
      * if a character is found that does not fit any of those classes it creates a new
      * array list called lexer error that stores the unknown characters
      * at the end it will return the character \o or null
      */
-    public static TokenStream lexString(String program) {
-        TokenStream tokens = new TokenStream();
-        String[] lines = program.split("\n");
-        String line;
-        int lineNum = 0;
+
+    function lexString(program) {
+
+        var tokens = new TokenStream();
+        var lines = program.split("\n");
+        var line;
+        var lineNum = 0;
         while (lineNum < lines.length) {
             line = lines[lineNum];
-            int currentColumn = 0;
+            var currentColumn = 0;
             while (currentColumn < line.length()) {
-                char currentChar = line.charAt(currentColumn);
+                var currentChar = line.charAt(currentColumn);
                 // Checks to see if this character is whitespace and creates a new token, the lexer then continues
                 // to look ahead in the string and advances the line and column numbers untill a charater is found 
                 // that isnt whitespace
                 if (Character.isWhitespace(currentChar)) {
-                    int whiteCol = currentColumn;
-                    String white = "" + currentChar;
+                    var whiteCol = currentColumn;
+                    var white = "" + currentChar;
                     while (Character.isWhitespace(currentChar = peek(line, currentColumn))) {
                         currentColumn++;
                         white = white + currentChar;
@@ -58,8 +60,8 @@ public class Lexer {
                 } // Multiple lines of comments are represented by multiple lines of tokens
                 // each full line will be one token
                 else if (currentChar == '-' && peek(line, currentColumn) == '-') {
-                    String com = "";
-                    int col = currentColumn;
+                    var com = "";
+                    var col = currentColumn;
                     while (currentColumn < line.length()) {
                         com = com + line.charAt(currentColumn);
                         currentColumn++;
@@ -69,33 +71,33 @@ public class Lexer {
                     break;
                 } // 
                 else if (Character.isLetter(currentChar)) {
-                    int varCol = currentColumn;
-                    String var = "" + currentChar;
+                    var varCol = currentColumn;
+                    var var1 = "" + currentChar;//variable called var, changed to var1
                     while (Character.isLetter(peek(line, currentColumn)) || Character.isDigit(peek(line, currentColumn))) {
                         currentColumn++;
                         currentChar = line.charAt(currentColumn);
-                        var = var + currentChar;
+                        var1 = var1 + currentChar;
                     }
-                    Symbol t = Symbol.toSymbol(var);
-                    TokenType ty = varToken;
+                    var t = Symbol.toSymbol(var1);
+                    var ty = varToken;
                     if (t == Symbol.ifKeyword || t == Symbol.thenKeyword || t == Symbol.elseKeyword) {
                         ty = puncToken;
                     }
                     
                     
-                    tokens.addToken(new Token(t, var, new Place(lineNum, varCol), ty));
+                    tokens.addToken(new Token(t, var1, new Place(lineNum, varCol), ty));
                 } else if (Character.isDigit(currentChar)) {
                     //allow 2.2 cant do 2. 
-                    int digitCol = currentColumn;
-                    String digit = "" + currentChar;
+                    var digitCol = currentColumn;
+                    var digit = "" + currentChar;
                     //This boolean logs if dec has ever been true
-                    boolean hasDecimal = false;
+                    var hasDecimal = false;
                     while (Character.isDigit(peek(line, currentColumn)) || '.' == peek(line, currentColumn)) {
                         //Setting true the fact that the number has at least one decimal
                         currentColumn++;
                         currentChar = line.charAt(currentColumn);
                         digit = digit + currentChar;
-                        boolean dot = '.' == peek(line, currentColumn);
+                        var dot = '.' == peek(line, currentColumn);
                         if (hasDecimal && dot) {
                             //error.add(new LexerError(currentChar + "", "that Decimal number has only one '.' ", lineNum, currentChar,fname));
                             break;
@@ -123,9 +125,9 @@ public class Lexer {
                 else if(";".contains(Character.toString(currentChar))) {
                     tokens.addToken(new Token(Symbol.toSymbol(Character.toString(currentChar)), Character.toString(currentChar), new Place(lineNum, currentColumn), semiToken));
                 }
-                else if (opChars.contains(Character.toString(currentChar))) {
-                    String op = Character.toString(currentChar);
-                    while (opChars.contains(Character.toString(peek(line, currentColumn)))) {
+                else if (OP_CHARS.contains(Character.toString(currentChar))) {
+                    var op = Character.toString(currentChar);
+                    while (OP_CHARS.contains(Character.toString(peek(line, currentColumn)))) {
                         currentColumn++;
                         currentChar = line.charAt(currentColumn);
                         op += currentChar;
@@ -143,10 +145,9 @@ public class Lexer {
         return tokens;
     }
 
-    private static char peek(String line, int colNum) {
+    function peek(line, colNum) {
         if (colNum + 1 < line.length()) {
             return line.charAt(colNum + 1);
         }
         return '\0';
     }
-}
