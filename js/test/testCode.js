75ce9cb7505225c7fdc6020d7f32679f9994c85b
Kellen Stuart <kellenstuart@Kellens-MacBook-Pro.local>
2016-02-10 16:54:41 -0700

    Adding lexer.js

--- js/test/testCode.js
+++ js/test/testCode.js
@@ -4,3 +4,4 @@
 ** This file is used for testing purposes.
 ** It is included in the index.html page.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+console.log("running testCode.js");
