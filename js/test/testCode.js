c23fab65a0229acf93882a05b99924b39851a854
unknown <stu715426@H101-02.wsc.western.edu>
2016-02-16 16:05:33 -0700

    Adding Project Files.

--- js/test/testCode.js
+++ js/test/testCode.js
@@ -5,25 +5,3 @@
 ** It is included in the index.html page.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 console.log("running testCode.js");
-function Base ( ) {
-  this.color = "blue";
-}
-
-function Sub ( ) {
-
-}
-Sub.prototype = new Base( );
-Sub.prototype.showColor = function ( ) {
- console.log( this.color );
-};
-
-Sub.prototype.print = function(aString) {
-	console.log(aString);
-};
-
-var instance = new Sub ( );
-instance.showColor( ); //"blue"
-instance.print("This is a string");
-
-var instance2 = new Sub();
-instance2.print("this is instance 2");
